4b758c0b4d274652799455a143b5dd39aa38effe
Maniek Gaznik <maniekgaznik@gmail.com>
2026-07-24 15:32:38 +0200

    13.2 done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
 
 import './styles/normalize.scss';
@@ -9,6 +11,8 @@ import 'font-awesome/css/font-awesome.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
