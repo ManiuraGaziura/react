00fe1ba446e2bc21506bc9837fa0864a39d0f434
Maniek Gaznik <maniekgaznik@gmail.com>
2026-07-24 16:11:25 +0200

    13.6 done

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,26 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import { updateSearchString } from '../../redux/store';
 
 const SearchForm = () => {
+  const [searchString, setSearchString] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    dispatch(updateSearchString(searchString));
+  };
+
   return (
-    <form className={styles.searchForm} onSubmit={event => event.preventDefault()}>
-      <TextInput placeholder="Search..." />
+    <form className={styles.searchForm} onSubmit={handleSubmit}>
+      <TextInput
+        value={searchString}
+        onChange={event => setSearchString(event.target.value)}
+        placeholder="Search..."
+      />
       <Button>
         <span className="fa fa-search" aria-hidden="true" />
         <span className={styles.visuallyHidden}>Search</span>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,23 @@
 import { createStore } from 'redux';
 import shortid from 'shortid';
 import initialState from './initialState';
+import strContains from '../utils/strContains';
+
+// selectors
+export const getFilteredCards = ({ cards, searchString }, columnId) =>
+  cards.filter(
+    card => card.columnId === columnId && strContains(card.title, searchString)
+  );
+
+export const getAllColumns = state => state.columns;
+
+// action creators
+export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
+export const addCard = payload => ({ type: 'ADD_CARD', payload });
+export const updateSearchString = payload => ({
+  type: 'UPDATE_SEARCHSTRING',
+  payload,
+});
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -20,6 +37,12 @@ const reducer = (state, action) => {
           ...state.cards,
           { ...action.payload, id: shortid() },
         ],
+      };
+
+    case 'UPDATE_SEARCHSTRING':
+      return {
+        ...state,
+        searchString: action.payload,
       };
 
     default:
